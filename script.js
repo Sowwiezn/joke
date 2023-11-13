9756e38a6cf5d9096f9ed6b1f0a238fe25b765aa
Sowwiezn <businka16.04.2007@gmail.com>
2023-11-13 18:20:57 +0200

    v4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-// const jokesContainer = document.getElementById('jokes_container');
+const jokesContainer = document.getElementById('jokes_container');
 
-// const jokeForm = document.getElementById('joke_form');
+const jokeForm = document.getElementById('joke_form');
 
-// let currentLength = 0;
+let currentLength = 0;
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://localhost:3000/jokes');
@@ -11,5 +11,30 @@ xhr.send();
 xhr.responseType = 'json';
 xhr.onload = () => {
     const jokes = xhr.response;
-    
+    if(jokes.light){
+        jokesContainer.innerHTML = ''
+        jokes.forEach(joke => {
+            jokesConteiner.innerHTML += getJokeHTML(joke)
+        });
+    }    
 };
+
+function getJokeHTML(joke){
+    return `
+        <div class="joke">
+        <div class="joke__content">
+            $(joke.content)
+        </div>
+        <div class="joke__footer">
+            <div class="joke__likes">
+                <span>$(joke.likes)</span>
+            </div>
+            </div>
+        <button class="joke__btn">
+            <span class="material-symbols-outlined">
+                thumb_up
+            </span>
+        </button>
+    </div>
+    `
+}
